5491682a717fba395a1ef04b94821deda7828294
bali.raghav@gmail.com <bali.raghav@gmail.com@b7506971-b9b3-6472-a3e5-f1f63b929ead>
2012-09-20 10:24:46 +0000

    [Raghav Bali : Sep 20 2012] 1. Updated narray_tree funtion return types to int 2. Created a new make file and made backup of earlier one 3. Added a new test file  to /test

---
+++ trunk/project/test/test_narry_func_ret.c
@@ -0,0 +1,49 @@
+/*******************************************************************
+Name            : Raghav Bali
+Description     : Test File for n_array_trees
+                  Updated the function return types to int
+                  from void
+Date            : Sep 20 2012
+Version         : NA
+*******************************************************************/
+
+#include "main.h"
+
+int main()
+{
+    narry_tree_t *head;
+    char filename[30];
+    file_descriptor_t *fd_temp;
+    file_descriptor_t fd_val,fd_val1,fd_val_head;
+
+    strcpy(fd_val.file_name,"raghav");
+    strcpy(fd_val_head.file_name,"/");
+
+    head=malloc(sizeof(narry_tree_t));
+    head->file_desc=&fd_val_head;
+    head->leftchild=NULL;
+    head->rightsibling=NULL;
+
+    //fd_temp=malloc(sizeof(fileDescriptor));
+    fd_temp=&fd_val;
+    printf("\n%p",&fd_val);
+    if (insert_node(&head,&fd_temp)!=TRUE)
+        printf("\n Node not inserted");
+    else
+        printf("\n Node inserted successfully");
+
+    printf("\n Enter a value : ");
+    gets(fd_val1.file_name);
+    fd_temp=&fd_val1;
+    printf("\n%p",&fd_val1);
+
+    if(insert_node(&head,&fd_temp)!=TRUE)
+        printf("\n Node not inserted");
+    else
+        printf("\n Node inserted successfully");
+
+  display(head);
+
+    return 0;
+}
+
