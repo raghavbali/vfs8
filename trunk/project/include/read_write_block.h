f6888a2e92ea3c4b11ee99efc85b8a423d622a03
pratibind.jha@gmail.com <pratibind.jha@gmail.com@b7506971-b9b3-6472-a3e5-f1f63b929ead>
2012-09-28 06:56:54 +0000

    "Header file 'read_write_block.h'  has been added. " .

--- trunk/project/include/read_write_block.h
+++ trunk/project/include/read_write_block.h
@@ -10,8 +10,9 @@ write data block into vfs
 fptr:  is vfs file pointer
 data_block:  is data that need to write into vfs
 offset: position in a vfs file where data will write.
+return succesfully writen or not.
 */
-int write_block(FILE *fptr,data_block_t data_block,int offset);
+int write_block(data_block_t data_block,int offset);
 
 
 /*
@@ -19,8 +20,9 @@ Read data block into vfs
 fptr:  is vfs file pointer
 data_block:  after reading the data block data will be in data_block
 offset: position in a vfs file where data have to read.
+return the block of data
 */
-const char *read_block(FILE *fptr,data_block_t data_block,int offset);
+data_block_t *read_block(int offset);
 
 /*
  End of File
