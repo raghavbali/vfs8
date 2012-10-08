8fa9c8a7e4bd38a33c2f947b9f178b0981d78775
bali.raghav@gmail.com <bali.raghav@gmail.com@b7506971-b9b3-6472-a3e5-f1f63b929ead>
2012-10-08 11:18:47 +0000

    [Raghav Bali : Oct 08 2012] Updates : 1. Update narray_tree.h to include new methods for create_node, create_file_descriptor     free_tree and tokenizer 2. Added method definitions for the above mentioned methods to narray_tree.c 3. Performed memory profiling to reduce memory leaks (current leak status at 48  bytes,    down from 1552 bytes) 4. Added a test file with the above mentioned changes 5. Updated makefile 6. Added a mem_check.txt file for memory profiling using valgrind.

--- trunk/project/include/narray_tree.h
+++ trunk/project/include/narray_tree.h
@@ -3,9 +3,11 @@
 * Description		:	n-arry tree on filenames(with paths)
 *                       having left child and right siblings.
 * Author		    :	Raghav Bali
-* Date			    :	Sep 7 2012
-* Version		    :	0.2
-* Updates           :   [Raghav Bali Sep 20 2012]: Updated Data Structures to use file_descriptors
+* Date			    :	Oct 08 2012
+* Version		    :	0.3
+* Updates           :   [Raghav Bali Oct 08 2012]: Added new methods : create_node, create_file_descriptor
+                        tokenizer and free_tree
+                        [Raghav Bali Sep 20 2012]: Updated Data Structures to use file_descriptors
                         [Raghav Bali Sep 20 2012]: Updated the formatting to Indian Hill
 **************************************************************************************************************/
 
@@ -35,3 +37,35 @@ int insert_node(narry_tree_t **head,file_descriptor_t **file_desc);
 * Display the tree data
 */
 void display(narry_tree_t  *head);
+
+/*
+*   Description :   This function creates a new node
+*   Input       :   Node to be created, file descriptor to be attached
+*   Output      :   True(1) if successful, FALSE(0) otherwise
+*/
+int create_node(narry_tree_t **node,file_descriptor_t **file_desc);
+
+/*
+*   Description :   This function creates a new file descriptor node
+*   Input       :   File descriptor to be created, file_name, loc number
+*   Output      :   True(1) if successful, FALSE(0) otherwise
+*/
+int create_file_descriptor(file_descriptor_t **file_desc,char * str,int loc_number);
+
+
+/*
+*   Description :   Tokenizes the node's name and inserts with the help of insert_node()
+*                   file_descriptor.loc_number = -1 signifies dummy node
+*                   file_descriptor.loc_number = -2 signifies root node
+*   Input       :   Head Node, file descriptor to be attached
+*   Output      :   NA
+*/
+int tokenizer(narry_tree_t **head,file_descriptor_t **file_desc);
+
+
+/*
+*   Description :   Recursively deallocate the tree nodes before unmounting
+*   Input       :   Head Node
+*   Output      :   NA
+*/
+void free_tree(narry_tree_t  *head);
