b796fa43d35342d4c8d34a2ce1198369f07643f1
bali.raghav@gmail.com <bali.raghav@gmail.com@b7506971-b9b3-6472-a3e5-f1f63b929ead>
2012-10-23 11:00:23 +0000

    [Raghav Bali : Oct 24 2012] Integrated with integrate.c

--- trunk/project/include/read_write_block.h
+++ trunk/project/include/read_write_block.h
@@ -6,23 +6,23 @@
 ****************************************************************************/
 
 /*
-write data block into vfs 
+write data block into vfs
 fptr:  is vfs file pointer
 data_block:  is data that need to write into vfs
 offset: position in a vfs file where data will write.
 return succesfully writen or not.
 */
-int write_block(data_block_t *data_block,int offset);
+int write_block(data_block_t *data_block,int metaheader_size,int offset);
 
 
 /*
-Read data block into vfs 
+Read data block into vfs
 fptr:  is vfs file pointer
 data_block:  after reading the data block data will be in data_block
 offset: position in a vfs file where data have to read.
 return the block of data
 */
-data_block_t *read_block(int offset);
+data_block_t *read_block(int metaheader_size,int offset);
 
 
 /*
