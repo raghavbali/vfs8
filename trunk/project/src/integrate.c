c21d6180c6f3dbf04459d183364accc6275e74e0
bali.raghav@gmail.com <bali.raghav@gmail.com@b7506971-b9b3-6472-a3e5-f1f63b929ead>
2012-10-23 11:01:33 +0000

    [Raghav Bali : Oct 24 2012] Added write file functionality

--- trunk/project/src/integrate.c
+++ trunk/project/src/integrate.c
@@ -20,7 +20,7 @@ int get_vfs_details()
 {
     printf("\n Enter VFS Label: ");
     scanf("%s",vfs_header.label_name);
-    printf("\n Enter VFS size (in MBs) :");
+    printf("\n Enter VFS size (in KBs) :");
     scanf("%ld",&vfs_header.vfs_size);
 }
 
@@ -30,10 +30,12 @@ int get_vfs_details()
     sets metaheader's :
         labelName and size fields
  */
-void get_directory_details(char name[])
+void get_directory_details(char name[],char path[])
 {
     printf("\n Enter directory name: ");
     scanf("%s",name);
+    printf("\n Enter directory path: ");
+    scanf("%s",path);
     //return name;
 }
 
@@ -54,6 +56,19 @@ void perform_action(int choice)
 
     data_block_t *test_data_block;
     char name[30];
+    char path[100];
+    char src[21];//="/raghav/bali/etc/xyz";
+    char dest[21];//="/bin";
+    char dest_dir_path[100];
+    char file_name[100];
+    char ubuntu_file_name[100];
+
+    strcpy(dest_dir_path,"/raghav");
+    strcpy(file_name,"abc.text");
+    strcpy(ubuntu_file_name,"../test/MirrorFile.txt");
+
+    strcpy(src,"/xyz");
+    strcpy(dest,"/raghav");
     int option=0;
     do
     {
@@ -61,7 +76,7 @@ void perform_action(int choice)
         {
         case 1:
             update_free_list();
-            if(unmount_vfs())
+            if(unmount_vfs(vfs_header.label_name))
             {
                 free(free_list);
                 free(file_descriptors);
@@ -77,8 +92,8 @@ void perform_action(int choice)
             break;
 
         case 2:
-            get_directory_details(name);
-            if(insert_tokenized_file_descriptor(name,1))
+            get_directory_details(name,path);
+            if(insert_tokenized_file_descriptor(name,path,1))
             {
                 printf("\n directory %s created successfully\n",name);
             }
@@ -90,7 +105,7 @@ void perform_action(int choice)
             break;
 
         case 3:
-            get_directory_details(name);
+            get_directory_details(name,path);
             if(del(name,1))
             {
                 printf("\n directory %s deleted successfully\n",name);
@@ -103,7 +118,8 @@ void perform_action(int choice)
             break;
 
         case 4:
-            printf("\n Under Construction ");
+            //printf("\n Under Construction ");
+            move_dir(src,dest);
             option=0;
             break;
 
@@ -116,7 +132,7 @@ void perform_action(int choice)
 
         case 6:
             /* logic for taking data either from file or creating file in console */
-            if(write_block(test_data_block,1024))
+            if(create_file(dest_dir_path,file_name,ubuntu_file_name))
                 printf("Data block has been writen successfully.\n");
             else
             {
@@ -151,9 +167,19 @@ void perform_action(int choice)
             option=0;
             break;
         default:
-            printf("\n Enter valid choice");
-            option=0;
-            break;
+            if(choice==30)
+            {
+                fd_array_dump('1');
+                option=0;
+                break;
+            }
+            else
+            {
+
+                printf("\n Enter valid choice");
+                option=0;
+                break;
+            }
 
         }
         if(!option)
@@ -183,16 +209,16 @@ void main()
         if(choice1==1)
         {
             get_vfs_details();
-            if(create_vfs())
+            if(create_vfs(vfs_header.label_name,vfs_header.vfs_size))
             {
-                printf("\n VFS %s of size %ld MB created successfully\n",vfs_header.label_name,vfs_header.vfs_size);
+                printf("\n VFS %s of size %ld KB created successfully\n",vfs_header.label_name,vfs_header.vfs_size);
                 init_tree();
                 choice2 = get_choice_from_user(); //getting data from the user
                 perform_action(choice2);  //performing action as per choice
             }
             else
             {
-                printf("\n VFS %s of size %ld MB could not be created\n",vfs_header.label_name,vfs_header.vfs_size);
+                printf("\n VFS %s of size %ld KB could not be created\n",vfs_header.label_name,vfs_header.vfs_size);
                 choice1=0;//loop back
             }
         }
@@ -205,29 +231,29 @@ void main()
             {
                 printf("\n\n The file label after mounting is : %s , \n The meta header is of size %ld\n",vfs_header.label_name,sizeof(vfs_header));
                 /* create root node in tree if this is the first directory/file */
-                        init_tree();
-                        choice2 = get_choice_from_user(); //getting data from the user
-                        perform_action(choice2);  //performing action as per choice
-                    }
-                    else
-                    {
-                        printf("Failed to mount "); // case of failure of mount function
-                        choice1=0;//loop back
-                    }
-
-                }
-                else if(choice1==3)
-                {
-                    exit(2);
-                }
-
-                else
-                {
-                    printf("\n Enter valid choice \n ");
-                }
-
-
+                init_tree();
+                choice2 = get_choice_from_user(); //getting data from the user
+                perform_action(choice2);  //performing action as per choice
             }
-            while(choice1<1 || choice1>3);
+            else
+            {
+                printf("Failed to mount "); // case of failure of mount function
+                choice1=0;//loop back
+            }
 
         }
+        else if(choice1==3)
+        {
+            exit(2);
+        }
+
+        else
+        {
+            printf("\n Enter valid choice \n ");
+        }
+
+
+    }
+    while(choice1<1 || choice1>3);
+
+}
