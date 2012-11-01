0bbf798ea07eae721d0232d3179bb1e795614b1f
pushpendrasinha20@gmail.com <pushpendrasinha20@gmail.com@b7506971-b9b3-6472-a3e5-f1f63b929ead>
2012-11-01 17:49:38 +0000

    [pushpendra]

--- trunk/project/src/vfs.c
+++ trunk/project/src/vfs.c
@@ -10,9 +10,9 @@
 #include "../include/main.h"
 
 /*
-    Create VFS in MBs
-    returns 0 on failure to create
-    returns 1 on success
+    Create VFS in KBs
+    returns 1,2,3,4 and 5 on failure to create,
+    returns SUCCESS(1000) on success,
 */
 int create_vfs(char *P1, int P2)
 {
@@ -22,50 +22,66 @@ int create_vfs(char *P1, int P2)
     long int block_size=1024;
 
     strcpy(vfs_header.label_name,P1);
-    vfs_header.vfs_size=P2;
 
-    /* Multiply block size to user defined size of VFS */
-    block_size=(block_size*vfs_header.vfs_size)+sizeof(vfs_header);
-    max_file_descriptors=vfs_header.vfs_size;//*1024;
-
-    /* Create VFS */
-    if((vfs_file=fopen(vfs_header.label_name,"wb+"))==NULL)
-    {
-        /* failure */
-        return FALSE;
-    }
+    /* Check if similar named  */
+    if(access( vfs_header.label_name, F_OK ) != -1 )
+        return 1;
     else
     {
-        /* secure file size by writing data at the last byte of the file */
-        fseek(vfs_file,block_size-1, SEEK_SET);
-        fwrite(&data_block,sizeof(data_block),sizeof(data_block)*block_size,vfs_file);
-
-        /* reset to beginning of file */
-        rewind(vfs_file);
-        /* initialize freelist */
-        free_list=(char *)malloc(sizeof(char)*max_file_descriptors);
-
-        for(i=0; i<max_file_descriptors/*MAXFILEDESCRIPTORS*/; i++)
+        /* Create VFS */
+        if((vfs_file=fopen(vfs_header.label_name,"wb+"))==NULL)
         {
-            // vfs_header.free_list[i]='0';
-            free_list[i]='0';
+            /* could not create vfs */
+            return 2;
         }
+        else
+        {
+            /* cannot create VFS greate than 700MB */
+            if(P2>734003200)
+                return 3;
+            /* size cannot be less than equal to 0 */
+            else if(P2<=0)
+                return 5;
+            else
+            {
+                vfs_header.vfs_size=P2;
+                /* Multiply block size to user defined size of VFS */
+                block_size=(block_size*vfs_header.vfs_size)+sizeof(vfs_header);
+                max_file_descriptors=vfs_header.vfs_size;//*1024;
 
-        file_descriptors=(file_descriptor_t *)malloc(sizeof(file_descriptor_t)*max_file_descriptors);
+                /* secure file size by writing data at the last byte of the file */
+                fseek(vfs_file,block_size-1, SEEK_SET);
+                fwrite(&data_block,sizeof(data_block),sizeof(data_block)*block_size,vfs_file);
 
-        /* write back the meta header */
-        fwrite(&vfs_header,sizeof(vfs_header),1,vfs_file);
+                /* reset to beginning of file */
+                rewind(vfs_file);
+                /* initialize freelist */
+                free_list=(char *)malloc(sizeof(char)*max_file_descriptors);
 
-        /* newly added */
-        fwrite(free_list,sizeof(free_list)*max_file_descriptors,1,vfs_file);
-        fwrite(file_descriptors,sizeof(file_descriptors)*max_file_descriptors,1,vfs_file);
-        /* modification ends */
+                for(i=0; i<max_file_descriptors/*MAXFILEDESCRIPTORS*/; i++)
+                {
+                    // vfs_header.free_list[i]='0';
+                    free_list[i]='0';
+                }
 
-        //free(free_list);
-        //free(file_descriptors);
-        fclose(vfs_file);
-        /* success */
-        return TRUE;
+                file_descriptors=(file_descriptor_t *)malloc(sizeof(file_descriptor_t)*max_file_descriptors);
+
+                /* write back the meta header */
+                fwrite(&vfs_header,sizeof(vfs_header),1,vfs_file);
+
+                /* newly added */
+                fwrite(free_list,sizeof(free_list)*max_file_descriptors,1,vfs_file);
+                fwrite(file_descriptors,sizeof(file_descriptors)*max_file_descriptors,1,vfs_file);
+                /* modification ends */
+
+                //free(free_list);
+                //free(file_descriptors);
+                fclose(vfs_file);
+
+                /* success */
+                return SUCCESS;
+            }
+        }
     }
 
 }
@@ -74,39 +90,44 @@ int create_vfs(char *P1, int P2)
 
 /*
     Mount VFS
-    returns 0 on failure to mount
-    return 1 on successful mount
+    returns 1,2,3 on failure to mount
+    return SUCCESS(1000) on successful mount
 */
 int mount_vfs(char vfs_name[])
 {
-
-    if((vfs_file=fopen(vfs_name,"rb+"))==NULL)
-    {
-        /* failure to mount */
-        return FALSE;
-    }
+    if(access( vfs_name, F_OK ) == -1 )
+        return 1;
     else
     {
-        /* start from 1st byte of the file */
-        strcpy(vfs_header.label_name,vfs_name);
-        rewind(vfs_file);
-        /* load the meta-header */
-        fread(&vfs_header,sizeof(vfs_header),1,vfs_file);
-        max_file_descriptors=vfs_header.vfs_size*1024;
 
-        /* newly added code */
-        free_list=(char *)malloc(sizeof(char)*max_file_descriptors);
-        file_descriptors=(file_descriptor_t *)malloc(sizeof(file_descriptor_t)*max_file_descriptors);
-        fread(free_list,sizeof(char)*max_file_descriptors,1,vfs_file);
-        fread(file_descriptors,sizeof(file_descriptor_t)*max_file_descriptors,1,vfs_file);
+        if((vfs_file=fopen(vfs_name,"rb+"))==NULL)
+        {
+            /* failure to mount */
+            return 2;
+        }
+        else
+        {
+            /* start from 1st byte of the file */
+            strcpy(vfs_header.label_name,vfs_name);
+            rewind(vfs_file);
+            /* load the meta-header */
+            fread(&vfs_header,sizeof(vfs_header),1,vfs_file);
+            max_file_descriptors=vfs_header.vfs_size;/**1024;*/
 
-        //free(free_list);
-        //free(file_descriptors);
-        /* modification eds */
+            /* newly added code */
+            free_list=(char *)malloc(sizeof(char)*max_file_descriptors);
+            file_descriptors=(file_descriptor_t *)malloc(sizeof(file_descriptor_t)*max_file_descriptors);
+            fread(free_list,sizeof(char)*max_file_descriptors,1,vfs_file);
+            fread(file_descriptors,sizeof(file_descriptor_t)*max_file_descriptors,1,vfs_file);
 
-        fclose(vfs_file);
-        /* successfully loaded */
-        return TRUE;
+            //free(free_list);
+            //free(file_descriptors);
+            /* modification eds */
+
+            fclose(vfs_file);
+            /* successfully loaded */
+            return SUCCESS;
+        }
     }
 }
 
@@ -119,10 +140,17 @@ int mount_vfs(char vfs_name[])
 */
 int unmount_vfs(char name[])
 {
-    if(!strcmp(name,vfs_header.label_name) && ((vfs_file=fopen(vfs_header.label_name,"rb+"))==NULL))
+    /* err out if file does not exist */
+    if(access( name, F_OK ) == -1 )
+        return 1;
+    /* err out if access is denied*/
+    if(access( name, R_OK & W_OK ) == -1 )
+        return 2;
+    /* err out if cannot write */
+    if((vfs_file=fopen(name,"rb+"))==NULL)
     {
-        /* failure */
-        return FALSE;
+        /* failure to write to file*/
+        return 3;
     }
     else
     {
@@ -137,8 +165,9 @@ int unmount_vfs(char name[])
 
         fclose(vfs_file);
         /* succefully written the data */
-        return TRUE;
+        return SUCCESS;
     }
 }
+
 
 
