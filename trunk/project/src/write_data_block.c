cdc20a9767a375b7fb68832923d10f8b29115d52
pushpendrasinha20@gmail.com <pushpendrasinha20@gmail.com@b7506971-b9b3-6472-a3e5-f1f63b929ead>
2012-11-01 17:48:28 +0000

    [pushpendra]

--- trunk/project/src/write_data_block.c
+++ trunk/project/src/write_data_block.c
@@ -7,57 +7,69 @@
 #include "../include/main.h"
 
 //write the data block in file system
-int write_block(data_block_t *data_block,int metaheader_size,int offset) {
-	FILE *fptr;
-	if((fptr=fopen(vfs_header.label_name,"rb+")) == NULL) {
-		//printf("Error occured in file opening\n");
-		return FALSE;
-	}
-	rewind(fptr);
-	fseek(fptr,(sizeof(data_block_t)*offset)+metaheader_size,SEEK_SET);
-	fwrite(data_block,sizeof(data_block_t),1,fptr);
-	fclose(fptr);
-	return TRUE;
+int write_block(data_block_t *data_block,int metaheader_size,int offset)
+{
+    FILE *fptr;
+    if((fptr=fopen(vfs_header.label_name,"rb+")) == NULL)
+    {
+        //printf("Error occured in file opening\n");
+        return FALSE;
+    }
+    if(data_block!=NULL)
+    {
+        rewind(fptr);
+        fseek(fptr,(sizeof(data_block_t)*offset)+metaheader_size,SEEK_SET);
+        fwrite(data_block,sizeof(data_block_t),1,fptr);
+        fclose(fptr);
+        return TRUE;
+    }
+    else
+        return FALSE;
 
 }
 
 //read the data block in file system and return the whole block as structure
- data_block_t *read_block(int metaheader_size,int offset) {
-	FILE *fptr;
-	data_block_t *data_block_read;
-	data_block_read =  (data_block_t *)malloc(sizeof(data_block_t));
-	if((fptr=fopen(vfs_header.label_name,"rb")) == NULL) {
-		//printf("Error occured in file opening: file can't read\n");
-		return NULL;
-	}
-	rewind(fptr);
-	fseek(fptr,(sizeof(data_block_t)*offset)+ metaheader_size,SEEK_SET);
-	fread(data_block_read,sizeof(data_block_t),1,fptr);
-	fclose(fptr);
-	return data_block_read ;
+data_block_t *read_block(int metaheader_size,int offset)
+{
+    FILE *fptr;
+    data_block_t *data_block_read;
+    data_block_read =  (data_block_t *)malloc(sizeof(data_block_t));
+    if((fptr=fopen(vfs_header.label_name,"rb")) == NULL)
+    {
+        //printf("Error occured in file opening: file can't read\n");
+        return NULL;
+    }
+    rewind(fptr);
+    fseek(fptr,(sizeof(data_block_t)*offset)+ metaheader_size,SEEK_SET);
+    fread(data_block_read,sizeof(data_block_t),1,fptr);
+    fclose(fptr);
+    return data_block_read ;
 
 }
 
 //read the text from user file and store in data_block for further use.
 
-data_block_t *read_text_from_user_file ( char filename[] ) {
+data_block_t *read_text_from_user_file ( char filename[] )
+{
 
-	FILE *fptr;
-	data_block_t *data_read;
-	data_read = (data_block_t *)malloc(sizeof(data_block_t));
-	(data_read->buffer_size,"");
-	char line[131];
-	strcpy ( data_read->buffer_size,"" ) ;
-	if ( (fptr=fopen(filename,"r")) == NULL ) {
-		return NULL;
-	}
+    FILE *fptr;
+    data_block_t *data_read;
+    data_read = (data_block_t *)malloc(sizeof(data_block_t));
+    (data_read->buffer_size,"");
+    char line[131];
+    strcpy ( data_read->buffer_size,"" ) ;
+    if ( (fptr=fopen(filename,"r")) == NULL )
+    {
+        return NULL;
+    }
 
-	rewind(fptr);
+    rewind(fptr);
 
-	//reading the line by line text form file.
-	while ( fgets ( line, sizeof line, fptr ) != NULL ) {
-		strcat(data_read->buffer_size,line);
-	}
-	fclose(fptr);
-	return data_read;
+    //reading the line by line text form file.
+    while ( fgets ( line, sizeof line, fptr ) != NULL )
+    {
+        strcat(data_read->buffer_size,line);
+    }
+    fclose(fptr);
+    return data_read;
 }
