bdc73a7369c70a4d9f530a3d76cd46679cbc2177
pushpendrasinha20@gmail.com <pushpendrasinha20@gmail.com@b7506971-b9b3-6472-a3e5-f1f63b929ead>
2012-11-02 17:10:21 +0000

    [pushpendra]

--- trunk/project/src/vfs_errorcodes.c
+++ trunk/project/src/vfs_errorcodes.c
@@ -3,7 +3,7 @@
 
 void console_output(int module, int index,char *output)
 {
-    error_flag=1; /* setting to error state */
+    //error_flag=1; /* setting to error state */
 
     /* CreateVFS */
     if(CREATEVFS==module)
@@ -11,18 +11,23 @@ void console_output(int module, int index,char *output)
         {
         case 1  :
             printf("createvfs_FAILURE: %s\n",ERR_VFS_CREATE_01);
+            error_flag=1;
             break;
         case 2  :
             printf("createvfs_FAILURE: %s\n",ERR_VFS_CREATE_02);
+            error_flag=1;
             break;
         case 3  :
             printf("createvfs_FAILURE: %s\n",ERR_VFS_CREATE_03);
+            error_flag=1;
             break;
         case 4  :
             printf("createvfs_FAILURE: %s\n",ERR_VFS_CREATE_04);
+            error_flag=1;
             break;
         case 5  :
             printf("createvfs_FAILURE: %s\n",ERR_VFS_CREATE_05);
+            error_flag=1;
             break;
         default :
             error_flag=0; /* not in error */
@@ -35,12 +40,15 @@ void console_output(int module, int index,char *output)
         {
         case 1  :
             printf("mountvfs_FAILURE: %s\n",ERR_VFS_MOUNT_01);
+            error_flag=1;
             break;
         case 2  :
             printf("mountvfs_FAILURE: %s\n",ERR_VFS_MOUNT_02);
+            error_flag=1;
             break;
         case 3  :
             printf("mountvfs_FAILURE: %s\n",ERR_VFS_MOUNT_03);
+            error_flag=1;
             break;
         default :
             error_flag=0; /* not in error */
@@ -53,12 +61,15 @@ void console_output(int module, int index,char *output)
         {
         case 1  :
             printf("unmountvfs_FAILURE: %s\n",ERR_VFS_UNMOUNT_01);
+            error_flag=1;
             break;
         case 2  :
             printf("unmountvfs_FAILURE: %s\n",ERR_VFS_UNMOUNT_02);
+            error_flag=1;
             break;
         case 3  :
             printf("unmountvfs_FAILURE: %s\n",ERR_VFS_UNMOUNT_03);
+            error_flag=1;
             break;
         default :
             error_flag=0; /* not in error */
@@ -105,7 +116,7 @@ void console_output(int module, int index,char *output)
             break;
         }
 
-        /* MoveDIR */
+    /* MoveDIR */
     if(MOVEDIR==module)
         switch(index)
         {
@@ -129,5 +140,116 @@ void console_output(int module, int index,char *output)
             printf("movedir_SUCCESS\n");
             break;
         }
+    /* ListDIR */
+    else if(LISTDIR==module)
+        switch(index)
+        {
+        case 1  :
+            printf("listdir_FAILURE: %s\n",ERR_VFS_LISTDIR_01);
+            break;
+        case 2  :
+            printf("listdir_FAILURE: %s\n",ERR_VFS_LISTDIR_02);
+            break;
+        case 3  :
+            printf("listdir_FAILURE: %s\n",ERR_VFS_LISTDIR_03);
+            break;
+        default :
+            error_flag=0; /* not in error */
+            printf("listdir_SUCCESS\n");
+            break;
+        }
 
+    /* AddFile */
+    if(ADDFILE==module)
+        switch(index)
+        {
+        case 1  :
+            printf("addfile_FAILURE: %s\n",ERR_VFS_ADDFILE_01);
+            break;
+        case 2  :
+            printf("addfile_FAILURE: %s\n",ERR_VFS_ADDFILE_02);
+            break;
+        case 3  :
+            printf("addfile_FAILURE: %s\n",ERR_VFS_ADDFILE_03);
+            break;
+        case 4  :
+            printf("addfile_FAILURE: %s\n",ERR_VFS_ADDFILE_04);
+            break;
+        default :
+            error_flag=0; /* not in error */
+            printf("addfile_SUCCESS\n");
+            break;
+        }
+
+    /* removeFile */
+    if(REMOVEFILE==module)
+        switch(index)
+        {
+        case 1  :
+            printf("removefile_FAILURE: %s\n",ERR_VFS_REMOVEFILE_01);
+            break;
+        default :
+            error_flag=0; /* not in error */
+            printf("removefile_SUCCESS\n");
+            break;
+        }
+
+    /* MoveFile */
+    if(MOVEFILE==module)
+        switch(index)
+        {
+        case 1  :
+            printf("movefile_FAILURE: %s\n",ERR_VFS_MOVEFILE_01);
+            break;
+        case 2  :
+            printf("movefile_FAILURE: %s\n",ERR_VFS_MOVEFILE_02);
+            break;
+        case 3  :
+            printf("movefile_FAILURE: %s\n",ERR_VFS_MOVEFILE_03);
+            break;
+        case 4  :
+            printf("movefile_FAILURE: %s\n",ERR_VFS_MOVEFILE_04);
+            break;
+        case 5  :
+            printf("movefile_FAILURE: %s\n",ERR_VFS_MOVEFILE_05);
+            break;
+        default :
+            error_flag=0; /* not in error */
+            printf("movefile_SUCCESS\n");
+            break;
+        }
+
+    /* MoveFile */
+    if(COPYFILE==module)
+        switch(index)
+        {
+        case 1  :
+            printf("copyfile_FAILURE: %s\n",ERR_VFS_COPYFILE_01);
+            break;
+        case 2  :
+            printf("copyfile_FAILURE: %s\n",ERR_VFS_COPYFILE_02);
+            break;
+        case 3  :
+            printf("copyfile_FAILURE: %s\n",ERR_VFS_COPYFILE_03);
+            break;
+        case 4  :
+            printf("copyfile_FAILURE: %s\n",ERR_VFS_COPYFILE_04);
+            break;
+        case 5  :
+            printf("copyfile_FAILURE: %s\n",ERR_VFS_COPYFILE_05);
+            break;
+        case 6  :
+            printf("copyfile_FAILURE: %s\n",ERR_VFS_COPYFILE_06);
+            break;
+        case 7  :
+            printf("copyfile_FAILURE: %s\n",ERR_VFS_COPYFILE_07);
+            break;
+        case 8  :
+            printf("copyfile_FAILURE: %s\n",ERR_VFS_COPYFILE_08);
+            break;
+        default :
+            error_flag=0; /* not in error */
+            printf("movefile_SUCCESS\n");
+            break;
+        }
 }
