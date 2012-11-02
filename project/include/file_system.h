bcf09fdafd27ecbc141f094b893fd3f91ac74d7f
pushpendrasinha20 <pushpendrasinha20@gmail.com>
2012-11-02 17:07:08 +0000

    [pushpendra]

--- project/include/file_system.h
+++ project/include/file_system.h
@@ -3,8 +3,9 @@
 * Description		:	This file contains all the data structures required
 * Author		    :	Deepthi
 * Date			    :	Aug 28 2012
-* Version		    :	0.2
-* Updates           :   [Raghav Bali Oct 11 2012]: Added function prototypes
+* Version		    :	0.3
+* Updates           :   [Raghav Bali Oct 29 2012]: Added function prototypes
+                        [Raghav Bali Oct 11 2012]: Added function prototypes
                         [Raghav Bali Sep 20 2012]: Updated Data Structuress as required
                         [Raghav Bali Sep 20 2012]: Updated the formatting to Indian Hill
 **************************************************************************************************************/
@@ -19,6 +20,7 @@
 //#define SIZEOFFILETYPE 3
 #define TRUE 1
 #define FALSE 0
+#define SUCCESS 1000
 
 /* Number of file descriptors */
 typedef unsigned int block_size_t;
@@ -60,6 +62,7 @@ FILE *vfs_file; /* VFS File Name */
 block_size_t max_file_descriptors; /* file_descriptor count */
 char *free_list; /* Free list array */
 file_descriptor_t *file_descriptors; /* file descriptor array */
+int error_flag; /* 1-error state and 0-not in error state */
 
 
 
@@ -96,7 +99,19 @@ int insert_file_descriptor(char* name, char *path, int type,int index);
 *   Input       :   name of the directory, type : 1-directory and 2-file
 *   Output      :   index of the first free block or FALSE(0)
 */
+<<<<<<< .mine
 int search_free_list(char *name,char *path, int type);
+
+
+/*
+*   Description :   updates free list and markes all dummy nodes as free
+*   Input       :   NA
+*   Output      :   NA
+*/
+void update_free_list();
+=======
+int search_free_list(char *name,char *path, int type);
+>>>>>>> .r65
 
 
 /*
@@ -106,11 +121,41 @@ int search_free_list(char *name,char *path, int type);
 */
 int del(char *name,int type);
 
+
 /*
 *   Description :   list the directory structure
 *   Input       :   NA
 *   Output      :   NA
 */
-void list_directory();//char *name)
+int list_directory(char *dir_path,int flag,char *text_file_path);
+
+
+/*
+*   Description :   Move a directory from source path to destination path
+*   Input       :   Source directory path, destination directory path
+*   Output      :   True for Success and False for Failure
+*/
+int move_dir(char *src, char *dest);
+
+
+/*
+*   Description :   Create a file in vfs from a file in the main OS with the specified file name
+*   Input       :   Destination directory path, filename(to be created), filepath (to read data from)
+*   Output      :   True for Success and False for Failure
+*/
+int create_file(char *dest_dir_path,char *file_name,char *data_file_path);
+
+
+/*
+*   Description :   Copy a file from source path to destination path
+*   Input       :   Source file path, destination directory path
+*   Output      :   True for Success and False for Failure
+*/
+int copy_file(char *src, char *dest);
+
+
+
+/* This is a debuggin function to view FD array data */
+void fd_array_dump(char condition);
 
 /*	End of File	*/
