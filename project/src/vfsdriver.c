f47733c84c913e59f3e5eceb75aff0133e501dd8
deepthi.bhagi <deepthi.bhagi@gmail.com>
2012-11-12 12:31:04 +0000

    Changes : Nov 12 2012

--- project/src/vfsdriver.c
+++ project/src/vfsdriver.c
@@ -143,10 +143,12 @@ void createvfs ( char *P1, int P2 )
     {
         init_tree();
     }
+
     if(head)
         free_tree(head);
     free(free_list);
     free(file_descriptors);
+
 
 }
 
@@ -162,10 +164,14 @@ void mountvfs ( char *P1 )
      else
          printf(VFS_02_FAILURE);
      //printf("mountvfs_TO_BE_DONE\n");*/
-    console_output(MOUNTVFS,mount_vfs(P1),P1);
+    if(!mount_flag)
+        console_output(MOUNTVFS,mount_vfs(P1),P1);
+    else
+        console_output(MOUNTVFS,4,P1);
     if(!error_flag)
     {
         init_tree();
+        //display_hashdump();
     }
 
 }
@@ -173,16 +179,28 @@ void mountvfs ( char *P1 )
 void unmountvfs ( char *P1 )
 {
     /* Call the appropriate function with given arguments and display appropriate output on the screen */
-    if(!error_flag)
+    if(!error_flag && mount_flag)
         update_free_list();
 
-    console_output(UNMOUNTVFS,unmount_vfs(P1),P1);
-    if(!error_flag)
+    if(mount_flag)
     {
-        free_tree(head);
-        free(free_list);
-        free(file_descriptors);
+        console_output(UNMOUNTVFS,unmount_vfs(P1),P1);
+        if(!error_flag)
+        {
+            free_tree(head);
+            free(free_list);
+            free_hash();
+            free(file_descriptors);
+
+            /* Nullify all globals except flags */
+            head=NULL;
+            free_list=NULL;
+            file_descriptors=NULL;
+            vfs_file=NULL;
+        }
     }
+    else
+        console_output(UNMOUNTVFS,4,P1);
     /*
     update_free_list();
     if(unmount_vfs(P1))
@@ -209,7 +227,7 @@ void makedir ( char *P1, char *P2 )
         printf(VFS_04_FAILURE,P2,P1);
     //printf("makedir_TO_BE_DONE\n");
     */
-    if(!error_flag)
+    if(!error_flag && mount_flag)
         console_output(MAKEDIR,insert_tokenized_file_descriptor(P2,P1,1),P1);
     else
         console_output(MAKEDIR,1,P1);
@@ -227,7 +245,7 @@ void deletedir ( char *P1 )
             printf(VFS_05_FAILURE,P1);
         //fd_array_dump('1');
     */
-    if(!error_flag)
+    if(!error_flag && mount_flag)
         console_output(DELDIR,del(P1,1),P1);
     else
         console_output(DELDIR,1,P1);
@@ -238,7 +256,7 @@ void movedir ( char *P1, char *P2 )
     /* Call the appropriate function with given arguments and display appropriate output on the screen */
     //move_dir(P1,P2);
     //printf("movedir_TO_BE_DONE\n");
-    if(!error_flag)
+    if(!error_flag && mount_flag)
         console_output(MOVEDIR,move_dir(P1,P2),P1);
     else
         console_output(MOVEDIR,1,P1);
@@ -250,7 +268,7 @@ void listdir ( char *P1, int P2, char *P3 )
     //printf("listdir_TO_BE_DONE\n");
     //list_directory(P1,P2,P3);
     //printf("\n");
-    if(!error_flag)
+    if(!error_flag && mount_flag)
         console_output(LISTDIR,list_directory(P1,P2,P3),P1);
     else
         console_output(LISTDIR,1,P1);
@@ -269,7 +287,7 @@ void addfile ( char *P1, char *P2, char *P3 )
         //return ;
     }
     */
-    if(!error_flag)
+    if(!error_flag && mount_flag)
         console_output(ADDFILE,create_file(P1,P2,P3),P1);
     else
         console_output(ADDFILE,1,P1);
@@ -278,13 +296,27 @@ void addfile ( char *P1, char *P2, char *P3 )
 void listfile ( char *P1, char *P2 )
 {
     /* Call the appropriate function with given arguments and display appropriate output on the screen */
-    printf("listfile_TO_BE_DONE\n");
+    //printf("listfile_TO_BE_DONE\n");
+
+    if(!error_flag && mount_flag)
+        console_output(LISTFILE,list_file(P1,P2,"w"),P1);
+    else
+        console_output(LISTFILE,1,P1);
 }
 
 void updatefile ( char *P1, char *P2 )
 {
     /* Call the appropriate function with given arguments and display appropriate output on the screen */
-    printf("updatefile_TO_BE_DONE\n");
+    if(!error_flag && mount_flag)
+        console_output(UPDATEFILE,update_file(P1,P2),P1);
+    else
+        console_output(UPDATEFILE,1,P1);
+
+
+    //data_block_t *data_block_read=read_block(sizeof(vfs_header)+sizeof(char)*max_file_descriptors+sizeof(file_descriptor_t)*max_file_descriptors,4);
+    // printf("\nData written :\n%s\n",data_block_read->buffer_size);
+
+    //printf("updatefile_TO_BE_DONE\n");
 }
 
 void removefile ( char *P1 )
@@ -297,7 +329,7 @@ void removefile ( char *P1 )
     else
         printf("\nCould not remove file");
         */
-    if(!error_flag)
+    if(!error_flag && mount_flag)
         console_output(REMOVEFILE,del(P1,2),P1);
     else
         console_output(REMOVEFILE,1,P1);
@@ -308,7 +340,7 @@ void movefile ( char *P1, char *P2 )
     /* Call the appropriate function with given arguments and display appropriate output on the screen */
     //move_dir(P1,P2);
     //printf("movefile_TO_BE_DONE\n");
-    if(!error_flag)
+    if(!error_flag && mount_flag)
         console_output(MOVEFILE,move_dir(P1,P2),P1);
     else
         console_output(MOVEFILE,1,P1);
@@ -327,7 +359,7 @@ void copyfile ( char *P1, char *P2 )
             //return ;
         }
     */
-    if(!error_flag)
+    if(!error_flag && mount_flag)
         console_output(COPYFILE,copy_file(P1,P2),P1);
     else
         console_output(COPYFILE,1,P1);
@@ -340,13 +372,42 @@ void copyfile ( char *P1, char *P2 )
 void exportfile ( char *P1, char *P2 )
 {
     /* Call the appropriate function with given arguments and display appropriate output on the screen */
-    printf("exportfile_TO_BE_DONE\n");
+    //printf("exportfile_TO_BE_DONE\n");
+
+    if(!error_flag && mount_flag)
+        console_output(EXPORTFILE,list_file(P1,P2,"wb"),P1);
+    else
+        console_output(EXPORTFILE,1,P1);
 }
 
 void searchfile ( char *P1, char *P2 )
 {
     /* Call the appropriate function with given arguments and display appropriate output on the screen */
-    printf("searchfile_TO_BE_DONE\n");
+    if(!error_flag && mount_flag)
+        console_output(SEARCHFILE,search_file(P1,P2),P1);
+    else
+        console_output(SEARCHFILE,3,P1);
+    /*
+    struct list *res;
+    res=(struct list *)malloc(sizeof(struct list));
+    res=search_files(P1);
+     if(res==NULL)
+            {
+                puts("FILE NOT FOUND");
+
+            }
+            else
+            {
+                printf("The files starting with %s are found in the paths \n",P1);
+                while(res!=NULL)
+                {
+                    printf("File %s in the path %s \n",res->file_desc->file_name,res->file_desc->loc_path);
+                    res=res->next;
+                }
+
+            }
+            */
+   // printf("searchfile_TO_BE_DONE\n");
 }
 
 
